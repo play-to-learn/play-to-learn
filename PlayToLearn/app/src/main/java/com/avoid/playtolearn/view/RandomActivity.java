3b868e2b32dbd5647bd1b5b5895f93f66468c75b
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-02-25 20:28:51 +0530

    no message

--- PlayToLearn/app/src/main/java/com/avoid/playtolearn/view/RandomActivity.java
+++ PlayToLearn/app/src/main/java/com/avoid/playtolearn/view/RandomActivity.java
@@ -2,14 +2,26 @@ package com.avoid.playtolearn.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.avoid.playtolearn.R;
 
 public class RandomActivity extends AppCompatActivity {
+    private Button throwButton;
+    private TextView diceValueTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random);
+
+        throwButton = (Button) findViewById(R.id.throw_button);
+        diceValueTextView = (TextView) findViewById(R.id.dice_value_text_view);
+    }
+
+    public void onClickThrowButton(View view) {
+
     }
 }
