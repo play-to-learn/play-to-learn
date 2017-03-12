a7ffa4b889f10efe9d9d932f08740844630e9c8a
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-03-13 01:38:03 +0530

    no message

---
+++ PlayToLearn/app/src/main/java/com/avoid/playtolearn/activity/MainMenuActivity.java
@@ -0,0 +1,15 @@
+package com.avoid.playtolearn.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.avoid.playtolearn.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
---
+++ PlayToLearn/app/src/main/java/com/avoid/playtolearn/activity/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.avoid.playtolearn.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.avoid.playtolearn.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
