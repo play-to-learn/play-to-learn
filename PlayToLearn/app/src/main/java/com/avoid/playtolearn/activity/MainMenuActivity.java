0e1ea4dc2085998dc17d0379f1ff8c2438333e68
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-03-17 00:36:04 +0530

    Link activities

--- PlayToLearn/app/src/main/java/com/avoid/playtolearn/activity/MainMenuActivity.java
+++ PlayToLearn/app/src/main/java/com/avoid/playtolearn/activity/MainMenuActivity.java
@@ -1,7 +1,9 @@
 package com.avoid.playtolearn.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.avoid.playtolearn.R;
 
@@ -11,5 +13,26 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+
+    public void onClickContinueButton(View view){
+        startActivity(new Intent(MainMenuActivity.this, BoardActivity.class));
+    }
+
+    public void onClickNewGameButton(View view){
+        startActivity(new Intent(MainMenuActivity.this, BoardActivity.class));
+    }
+
+    public void onClickOptionsButton(View view){
+        startActivity(new Intent(MainMenuActivity.this, SettingsActivity.class));
+    }
+
+    public void onClickProfileButton(View view){
+        startActivity(new Intent(MainMenuActivity.this, ProfileActivity.class));
+    }
+
+    public void onClickLeaderboardButton(View view){
+        startActivity(new Intent(MainMenuActivity.this, LeaderboardActivity.class));
     }
 }
