846acc1de62b20b79ae22936a23afc4f246c4277
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-03-19 16:16:59 +0530

    Create common main button

---
+++ PlayToLearn/app/src/main/java/com/avoid/playtolearn/widget/MainButton.java
@@ -0,0 +1,37 @@
+package com.avoid.playtolearn.widget;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+import com.avoid.playtolearn.R;
+
+/**
+ * Created by sumedhe on 3/19/17.
+ */
+
+public class MainButton extends android.support.v7.widget.AppCompatButton {
+    public MainButton(Context context){
+        super(context);
+        applyCustomShape(context);
+    }
+
+    public MainButton(Context context, AttributeSet attrs){
+        super(context, attrs);
+        applyCustomShape(context);
+    }
+
+    public MainButton(Context context, AttributeSet attrs, int defStyleAttrs){
+        super(context, attrs, defStyleAttrs);
+        applyCustomShape(context);
+    }
+
+    private void applyCustomShape(Context context){
+        setBackgroundResource(R.drawable.common_main_button);
+        setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/zantroke.otf"));
+        setTextSize(18);
+        setTextColor(Color.parseColor("#874f21"));
+    }
+
+}
