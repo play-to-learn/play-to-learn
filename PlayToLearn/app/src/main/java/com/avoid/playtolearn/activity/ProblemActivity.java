5f3d9beef89981374d5821b58b9c79772c081433
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-04-09 21:50:19 +0530

    Add Problem Activity

---
+++ PlayToLearn/app/src/main/java/com/avoid/playtolearn/activity/ProblemActivity.java
@@ -0,0 +1,14 @@
+package com.avoid.playtolearn.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.avoid.playtolearn.R;
+
+public class ProblemActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_problem);
+    }
+}
