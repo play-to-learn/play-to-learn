bc3d3f62a4f5fdb184e7b61238abc0e4d8117005
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-04-10 07:59:04 +0530

    Full screen board interface

--- Database/main_question.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO question (name, description, answer, score) VALUES ('One', 'Move one step', 1, 5);
-INSERT INTO question (name, description, answer, score) VALUES ('Two', 'Move two steps', 2, 10);
-INSERT INTO question (name, description, answer, score) VALUES ('Three', 'Move three steps', 3, 5);
