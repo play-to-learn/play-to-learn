f5c774a6ebd83d3c082abf9bc264535a2ffa3ed0
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-04-12 20:36:47 +0530

    Modify board

--- PlayToLearn/app/src/main/java/com/avoid/playtolearn/common/Global.java
+++ PlayToLearn/app/src/main/java/com/avoid/playtolearn/common/Global.java
@@ -1,6 +1,6 @@
 package com.avoid.playtolearn.common;
 
 public class Global {
-    public static int COLUMN_COUNT = 6;
-    public static int ROW_COUNT = 10;
+    public static int COLUMN_COUNT = 7;
+    public static int ROW_COUNT = 11;
 }
